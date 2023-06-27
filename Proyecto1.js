2276945f235d29a3afe69f712cde883f581189fc
Ingrid Caceres <ingridcaceres36@gmail.com>
2023-06-27 12:43:38 -0500

    Inicio del proyecto 1 en JS

--- Proyecto1.js
+++ Proyecto1.js
@@ -10,12 +10,19 @@
             <div id="post"> </div>
                 <h1> Database Project One </h1>
                 <p> You can use it with any three variables you want </p>
+                
                 <script> 
                 let.variable1 = "" 
                 variable1 = prompt ( " Enter variable No. 1 " )
-                alert ( "Variable No.1 you entered is " + variable1)
+                alert ( "Variable No.1 you entered is " + variable1 )
+                
+                let.variable2 = ""
+                variable2 = prompt ( " Enter variable No. 2 ")
+                alert ( " Variable No.2 you entered is " + variable2 )
 
+                let.variable3 = ""
+                variable3 = prompt ( " Enter variable No. 3 ")
+                alert ( " Variable No.3 you entered is " + variable3 )
                 </script>
-
     </body>
 </html>
