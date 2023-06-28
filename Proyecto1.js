ba09877916f2226d51da4ecd577a04b230e897af
Ingrid Caceres <ingridcaceres36@gmail.com>
2023-06-28 17:48:01 -0500

    ingreso de variables

--- Proyecto1.js
+++ Proyecto1.js
@@ -1,28 +1,18 @@
-<html>
-    <head>
-        <title> Project One </title>
-        <link rell="stylesheet" href="proyecto1/styles.css" />     
-    </head>    
-    <body>
-        <div id="container"> </div>
-            <div id="cabecera"> </div>
-               DATABASE
-            <div id="post"> </div>
-                <h1> Database Project One </h1>
-                <p> You can use it with any three variables you want </p>
-                
-                <script> 
-                let.variable1 = "" 
-                variable1 = prompt ( " Enter variable No. 1 " )
-                alert ( "Variable No.1 you entered is " + variable1 )
-                
-                let.variable2 = ""
-                variable2 = prompt ( " Enter variable No. 2 ")
-                alert ( " Variable No.2 you entered is " + variable2 )
+let variable1
+let variable2
+let variable3
 
-                let.variable3 = ""
-                variable3 = prompt ( " Enter variable No. 3 ")
-                alert ( " Variable No.3 you entered is " + variable3 )
-                </script>
-    </body>
-</html>
+
+function EnterVariables () {
+  variable1 = prompt ( " Enter variable No. 1 " )
+  alert ( "Variable No.1 you entered is " + variable1 )
+                
+  let variable2 = ""
+  variable2 = prompt ( " Enter variable No. 2 ")
+  alert ( " Variable No.2 you entered is " + variable2 )
+
+  let variable3 = ""
+  variable3 = prompt ( " Enter variable No. 3 ")
+  alert ( " Variable No.3 you entered is " + variable3 )
+
+}
